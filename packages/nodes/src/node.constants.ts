ad99c13229c0fa1652643eef34491392b8e4334e
itaven <vladk520@gmail.com>
2023-08-25 13:59:39 +0700

    test tester667

--- packages/nodes/src/node.constants.ts
+++ packages/nodes/src/node.constants.ts
@@ -10,6 +10,7 @@ export type EthereumNodeServiceOptions = {
   defaultBa111lance?: number;
   defaultBa111lan222ce?: number;
   defaultBa111lan222ce?: number;
+  defaultBa111lan222ce?: number;
 };
 
 export type Account = {
